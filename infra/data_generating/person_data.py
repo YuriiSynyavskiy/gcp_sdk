0d52e7c2e9fe163f8a9aefd799e7c175a3a2294e
Yurii <yurii@ysyniavskyi.nb.ipa.dataart.net>
2021-10-09 23:15:43 +0300

    divided person schema, added generator for persons' data

--- infra/data_generating/person_data.py
+++ infra/data_generating/person_data.py
@@ -7,6 +7,7 @@ from dotenv import load_dotenv
 from google.cloud import storage
 from datetime import datetime
 
+
 def phn():
     n = '0000000000'
     while '9' in n[3:6] or n[3:6]=='000' or n[6]==n[7]==n[8]==n[9]:
@@ -35,12 +36,38 @@ if __name__=="__main__":
                                      "start_date": f"{random.randint(1980, 2021)}-{random.randint(1,12)}-{random.randint(1,28)} 00:00:00"} for i in range(1,101))
                     
     else:
-        with open(args.file_path, 'r') as f_r:
-            person_reader = csv.reader(f_r, delimiter=',', quotechar='"')
-            updated_file_name = f"persons_{args.file_path.split('_')[-1]}_updated_{str(datetime.timestamp(datetime.now())).split('.')[0]}"
-            with open(updated_file_name, 'w') as f_w:
-                skip_header = next(person_reader)
-                for row in person_reader:
-                    import pdb; pdb.set_trace()
+        try:
+            with open(args.file_path, 'r') as f_r:
+                person_reader = csv.reader(f_r, delimiter=',', quotechar='"')
+                updated_file_name = f"persons_{args.file_path.split('_')[-1].split('.')[0]}_updated_{str(datetime.timestamp(datetime.now())).split('.')[0]}.csv"
+                with open(updated_file_name, 'w') as f_w:
+                    person_writer = csv.writer(f_w, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+                    person_writer.writerow(["id", "department_id", "position_id", "name", 
+                                               "surname", "salary", "phone", "start_date", "end_date"])
+                    skip_header = next(person_reader)
+                    for row in person_reader:
+                        if random.random() < 0.1:
+                            choice = random.randint(1,7)
 
+                            # Change dept_id
+                            if choice == 1:  
+                                row[1] = random.randint(1,20)
+                            # Change position_id
+                            elif choice == 2:            
+                                row[2] = random.randint(1,8)
+                            # Change name
+                            elif choice == 3:            
+                                row[3] = names.get_first_name()
+                            # Change surname
+                            elif choice == 4:
+                                row[4] = names.get_last_name()
+                            # Change salary
+                            elif choice == 5:
+                                row[5] = random.randint(2,14)*random.choice([1000, 1250])
+                            # Change phone
+                            elif choice == 6:
+                                row[6] = phn()
+                        person_writer.writerow(row)
+        except FileNotFoundError:
+            print("Incorrect file name!")
 
