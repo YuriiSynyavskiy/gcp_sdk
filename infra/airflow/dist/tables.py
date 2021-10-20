799b9415a063e4f66906b40cacb3d5d261b73f42
Yurii Syniavskyi <ysyniavskyi@universe.dart.spb@ysyniavskyi2.nb.ipa.dataart.net>
2021-10-20 11:21:20 +0300

    updated person scd dag, added logging

--- infra/airflow/dist/tables.py
+++ infra/airflow/dist/tables.py
@@ -1,7 +1,5 @@
-landing_person_table = 'landing_dm_person'
-staging_person_table = 'staging_dm_person'
-target_person_table = 'dm_person'
-
+tmp_person_table_name = 'tmp_dm_person'
+person_table_name = 'dm_person'
 
 tmp_dept_table_name = 'tmp_dm_department'
 dept_table_name = 'dm_department'
